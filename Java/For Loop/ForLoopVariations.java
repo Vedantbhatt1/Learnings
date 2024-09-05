5514972b305e5eada33225bfc55471b698ddb81c
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-09-05 18:20:28 +0530

    Add files via upload

---
+++ Java/For Loop/ForLoopVariations.java
@@ -0,0 +1,7 @@
+public class ForLoopVariations{
+    public static void main(String[] args) {
+        for(int i =1,j=1;i<=85 && j<=100;i++,j+=20){
+            System.out.println(i+" "+j);
+        }
+    }
+}

e67b96ec56ae4ea6e4234d72785371aad114e72a
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-09-05 18:17:44 +0530

    Create Logical_Relational_Operators.java

---
+++ Java/Conditionals and Loops/Logical_Relational_Operators.java
@@ -0,0 +1,13 @@
+public class Logical_Relational_Operators{
+    public static void main(String[] args) {
+        int a =10,b=10;
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+
+        System.out.println(true || false);
+        System.out.println(true && false);
+        System.out.println(!false);
+    }
+}

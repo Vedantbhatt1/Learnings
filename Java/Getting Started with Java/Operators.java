45a21297b01bd17a1d444b5a9dfdff6eadc1d8ac
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-09-05 18:10:01 +0530

    Add files via upload

---
+++ Java/Getting Started with Java/Operators.java
@@ -0,0 +1,10 @@
+public class Operators{
+    public static void main(String[] args) {
+        int a =10;
+        int b =12;
+        int sum = a+b;
+        int diff = a-b;
+        int div = a/b;
+        int mul = a*b;
+    }
+}
---
+++ Java/Getting Started with Java/Taking_Input.java
@@ -0,0 +1,24 @@
+
+public class Taking_Input{
+    public static void main(String[] args) {
+        // Scanner s = new Scanner(System.in);
+        // int a =s.nextInt();
+        // int b = s.nextInt();
+        // int c =a+b;
+        // System.out.println(c);
+
+        // Scanner s1 = new Scanner(System.in);
+        // // String str = s1.next();
+        // String str1 = s1.nextLine();
+        // System.out.println(str1);
+        
+        // Scanner s2 = new Scanner(System.in);
+        // String Str1 = s2.next();
+        // char chr = Str1.charAt(0);
+        // System.out.println(chr);
+
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        System.out.println(min + " " + max);
+    }
+}

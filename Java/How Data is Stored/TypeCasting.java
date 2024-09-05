887fc081f14ffb7f6681f83bc38f8e78f7d67f16
Vedant Bhatt <115866890+Vedantbhatt1@users.noreply.github.com>
2024-09-05 18:11:27 +0530

    Create TypeCasting.java

---
+++ Java/How Data is Stored/TypeCasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting{
+    public static void main(String[] args) {
+        // char c = 'a';
+        // int i = c;
+        // System.out.println(i);
+        // c = (char)i;
+        // System.out.println(c);
+
+        float f = (float)1.7;
+        System.out.println('a'+1);
+    }
+}
